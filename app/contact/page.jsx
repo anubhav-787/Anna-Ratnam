b96c29c5c09d63e23d19f25994e4ce4630689c85
anubhav-787 <anubhavdixit688@gmail.com>
2026-04-11 23:39:41 +0530

    Add overflow-x-auto class to contact page div

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className=' bg-fuchsia-200 flex'>
+    <div className=' bg-fuchsia-200 flex overflow-x-auto'>
       <div className='h-[100vh] w-1/2 text-5xl font-bold flex flex-col justify-center align-center ml-3.5 border-2 p-1.'>Contact Us</div>
       <div className='h-[100vh] w-1/2 flex flex-col justify-center align-center p-5 '>
         <p>We are available for questions, feedback, or collaboration opportunities. Let us know how we can help!</p>
