8069181d9e3d82185a69220e08feef953cbb62eb
anubhav-787 <anubhavdixit688@gmail.com>
2026-04-14 13:09:22 +0530

    Update contact email address in page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -6,7 +6,7 @@ const page = () => {
       <div className='h-[100vh] w-1/2 text-5xl font-bold flex flex-col justify-center align-center ml-3.5 border-2 p-1.'>Contact Us</div>
       <div className='h-[100vh] w-1/2 flex flex-col justify-center align-center p-5 '>
         <p>We are available for questions, feedback, or collaboration opportunities. Let us know how we can help!</p>
-        <p className='font-bold text-lg'>You can reach us at <span className='underline text-shadow-blue-600'>developeranubhavdixit99@gmail.com</span></p>
+        <p className='font-bold text-lg'>You can reach us at <span className='underline text-shadow-blue-600'>anubhavdixit688@gmail.com</span></p>
       </div>
     </div>
   )
